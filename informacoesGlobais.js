73a2e8914f421a8b2248461b09661693396ec5a1
julionagaita2 <111884078+julionagaita2@users.noreply.github.com>
2024-10-16 09:07:16 -0300

    Add files via upload

---
+++ informacoesGlobais.js
@@ -0,0 +1,14 @@
+const url = 'https://raw.githubsercontet.com/guilhermeonrails/api/main/dados-globas.json'
+async function visualizarInformacoesGlobais() {
+    const res= await fetch(url)
+    const dados = await res.json
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_de_pessoas_mundo / 1e9)
+    console.log(dados);
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${dados.total_de_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão em alguma rede social e passam em média <span>${dados.tempo_medio}</span>horas conectadas.`
+}
+
+
+visualizarInformacoesGlobais();
