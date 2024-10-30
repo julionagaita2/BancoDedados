cc20dc9d363866f3fd01723f86fe78bc099c0ec6
julionagaita2 <111884078+julionagaita2@users.noreply.github.com>
2024-10-30 09:12:02 -0300

    Add files via upload

--- quantidadeUsuariosPorRede.js
+++ quantidadeUsuariosPorRede.js
@@ -1,8 +1,10 @@
+import {getCSS, tickConfig} from "./common.js"
 async function quantidadeUsuariosPorRede() {
-    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json&#39'
+    const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch (url)
     const dados = await res.json()
     const nomeDasRedes = Object.keys(dados);
+    const quantidadeDeUsuarios = Object.values(dados)
     
     const data = [
         {
